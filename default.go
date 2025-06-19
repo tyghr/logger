8f5c22d6a0081da4dbcee973ce7549da2545c33b
Denis.Kuzmin <tyghr27@gmail.com>
2025-06-19 18:24:19 +0300

    fix default logger init

--- default.go
+++ default.go
@@ -29,21 +29,23 @@ func init() {
 func Init(opts ...Option) {
 	std = NewLogger(opts...)
 
-	Debug = std.Debug
-	Debugf = std.Debugf
-	Debugw = std.Debugw
-	Info = std.Info
-	Infof = std.Infof
-	Infow = std.Infow
-	Warn = std.Warn
-	Warnf = std.Warnf
-	Warnw = std.Warnw
-	Error = std.Error
-	Errorf = std.Errorf
-	Errorw = std.Errorw
-	Fatal = std.Fatal
-	Fatalf = std.Fatalf
-	Fatalw = std.Fatalw
-	FailOnError = std.FailOnError
-	Sync = std.Sync
+	if std != nil {
+		Debug = std.Debug
+		Debugf = std.Debugf
+		Debugw = std.Debugw
+		Info = std.Info
+		Infof = std.Infof
+		Infow = std.Infow
+		Warn = std.Warn
+		Warnf = std.Warnf
+		Warnw = std.Warnw
+		Error = std.Error
+		Errorf = std.Errorf
+		Errorw = std.Errorw
+		Fatal = std.Fatal
+		Fatalf = std.Fatalf
+		Fatalw = std.Fatalw
+		FailOnError = std.FailOnError
+		Sync = std.Sync
+	}
 }
